41c02aa9ea9efecd59359a85dcf2b2b9171ffdbc
xiaozhijie6 <2788834675@qq.com>
2026-07-25 03:48:59 +0800

    心情日历紧凑卡内容放大：日期/图标/文字全面升级，双卡文字同步微调

--- tools/api_push.py
+++ tools/api_push.py
@@ -3,7 +3,7 @@
 import base64, json, subprocess, sys
 
 REPO = "xiaozhijie6/xixi-demo"
-MSG = "心情日历改版：首页左卡（日期+今日情绪）右列（冥想/助眠上下排），玻璃垫图磨砂高短调，新增月历详细页"
+MSG = "心情日历紧凑卡内容放大：日期/图标/文字全面升级，双卡文字同步微调"
 CHANGED = ["demo/index.html", "demo/css/app.css", "demo/js/app.js", "tools/api_push.py"]
 ADDED = []
 DELETED = []
